0ecf8b2696d4a4221a8987eeda1ffb9c74f416f5
Richard Summer <richardforrestsummer@gmail.com>
2021-01-21 11:21:52 -0500

    Installed React

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g*6z@&5$w6u@2s_0hi)m=s^q!m8-l^jh1v_oyc(=)*6^(_6748'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
