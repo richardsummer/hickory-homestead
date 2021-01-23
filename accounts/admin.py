e595653fabdeefdec44e8265584dc9d4aad1a65c
Richard Summer <richardforrestsummer@gmail.com>
2021-01-22 20:38:07 -0500

    Corrected typo

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Profile
 
 admin.site.register(User, UserAdmin)
