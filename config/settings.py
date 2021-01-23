1aed7154f2ba198b638570d981d2b29d3ba6894a
Richard Summer <richardforrestsummer@gmail.com>
2021-01-22 20:06:45 -0500

    React showing on 8000

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +39,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #3rd Party
+
+    #Local
+    'frontend.apps.FrontendConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REACT_APP_DIR = os.path.join(BASE_DIR, 'frontend/static')
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'frontend/static/build/static'), )
